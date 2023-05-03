06a78368167dde01445869d648371971e3dc4ccd
BusterAlan <126498817+BusterAlan@users.noreply.github.com>
2023-05-03 09:53:53 -0600

    final version

--- barra_de_navegación.js
+++ barra_de_navegación.js
@@ -3,7 +3,7 @@ var navBar = document.getElementById('barraNav').innerHTML =
 `    
 <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #e3f2fd;">
 <div class="container-fluid">
-    <a class="navbar-brand" href="../HTML/main.html">Inicio</a>
+    <a class="navbar-brand" href="index.html">Inicio</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
